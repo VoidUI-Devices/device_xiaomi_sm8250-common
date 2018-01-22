15b7929273374527466bcc59abd048b3ce6c6b20
Kumar Anand <kumaranand@google.com>
2018-01-22 15:02:54 -0800

    sm8250-common: Enable automatic channel selection
     * Automatic Channel Selection algorithms and implementations
       are used to enable interfaces to automatically figure
       out which channel configuration to use for initiating
       communication, for any mode of operation which initiates
       radiation (AP, Mesh, IBSS, P2P).
    
       https://wireless.wiki.kernel.org/en/users/documentation/acs
    
    Change-Id: I5c95994ea52495990cf9244c703bc88db170f7f6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -246,5 +246,7 @@ WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
+CONFIG_ACS := true
+
 # Inherit the proprietary files
 include vendor/xiaomi/sm8250-common/BoardConfigVendor.mk
