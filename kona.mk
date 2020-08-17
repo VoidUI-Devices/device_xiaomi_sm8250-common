de628f96a321fbbafef7045f312a334aaa99dc5d
Yo Chiang <yochiang@google.com>
2020-08-17 11:44:54 +0800

    sm8250-common: Use on_vendor VNDK APEX
    Move VNDK APEX to /vendor partition so we don't need to update
    vendor.img prebuilt everytime there is a change in the VNDK
    libraries.

--- kona.mk
+++ kona.mk
@@ -528,6 +528,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
+# VNDK
+PRODUCT_PACKAGES += \
+    com.android.vndk.current.on_vendor
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
