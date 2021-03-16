bd5c8599c244e3cd6621d0a4ba58fae16ec38086
Pig <pig.priv@gmail.com>
2021-03-16 19:57:23 +0300

    sm8250-common: Move DPM feature property to /system_ext
    * DPM blobs are under /system_ext.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -162,6 +162,7 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
+TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 
 # Recovery
 ifeq ($(TARGET_IS_VAB),true)
