5aa796c5d978666d4c6fc96ca9dacb5f0c4c4e2a
Bruno Martins <bgcngm@gmail.com>
2021-07-10 12:57:54 +0300

    sm8250-common: Implement SunlightEnhancement LiveDisplay HAL
    - Additionally bringup Sepolicy for LiveDisplay hals

--- kona.mk
+++ kona.mk
@@ -302,7 +302,8 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service.alioth
 
 # Media
 PRODUCT_PACKAGES += \
