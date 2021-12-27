d09dc84b09388c4d1349574bffbada50d9865ece
dev-harsh1998 <harshit@hyperteam.co.in>
2021-12-27 17:50:24 +0100

    sm8250-common: BoardConfig: Enable HWUI_COMPILE_FOR_PERF
    - Activates few optimizing compiler flags for HWUI

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -114,6 +114,9 @@ DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 ODM_MANIFEST_SKUS += nfc
 ODM_MANIFEST_NFC_FILES := $(COMMON_PATH)/manifest_nfc.xml
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Init
 TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_xiaomi_kona
 TARGET_RECOVERY_DEVICE_MODULES ?= init_xiaomi_kona
