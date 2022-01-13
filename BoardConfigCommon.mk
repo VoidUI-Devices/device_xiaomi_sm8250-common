a88cf1ba198714530e626cf6f99ef857fc325e1e
Alin Jerpelea <alin.jerpelea@sony.com>
2022-01-13 08:48:15 +0000

    sm8250-common: BoardConfig: silence console output
    we silence the console output to reduce the spam in dmesg

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -127,6 +127,7 @@ endif
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.memcg=1 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm kpti=off
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_CMDLINE += console=null
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
