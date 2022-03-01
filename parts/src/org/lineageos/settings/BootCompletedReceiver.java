bb85cfaa4abad088b87aa8790141c35dd67e6596
lostark13 <tushar9915@gmail.com>
2022-03-01 12:52:54 -0300

    sm8250-common: parts: Add per app maximum refresh rate
      [GustavoMends]:
    * Adapt to alioth
    * Drop 30/50/90hz  (We do not support these values)

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,6 +23,7 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.lineageos.settings.thermal.ThermalUtils;
+import org.lineageos.settings.refreshrate.RefreshUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -33,5 +34,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         ThermalUtils.startService(context);
+        RefreshUtils.startService(context);        
     }
 }
