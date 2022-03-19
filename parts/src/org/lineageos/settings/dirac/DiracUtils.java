4e9e3d79e1c1e624d2562e0c4f964278ecd33dea
Lostark13 <tushar9915@gmail.com>
2022-03-19 19:20:57 +0300

    sm8250-common: parts: Add MiSound scenes

--- parts/src/org/lineageos/settings/dirac/DiracUtils.java
+++ parts/src/org/lineageos/settings/dirac/DiracUtils.java
@@ -130,4 +130,19 @@ public class DiracUtils {
     public void setHeadsetType(int paramInt) {
         mDiracSound.setHeadsetType(paramInt);
     }
+
+    public boolean getHifiMode() {
+        AudioManager audioManager = mContext.getSystemService(AudioManager.class);
+        return audioManager.getParameters("hifi_mode").contains("true");
+    }
+
+    public void setHifiMode(int paramInt) {
+        AudioManager audioManager = mContext.getSystemService(AudioManager.class);
+        audioManager.setParameters("hifi_mode=" + (paramInt == 1 ? true : false));
+        mDiracSound.setHifiMode(paramInt);
+    }
+
+    public void setScenario(int sceneInt) {
+        mDiracSound.setScenario(sceneInt);
+    }
 }
