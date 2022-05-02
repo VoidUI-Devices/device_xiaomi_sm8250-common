5c37b4236d3b52f84acd79ea6bb5c063fb63cd0f
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2022-05-02 03:26:03 +0000

    sm8250-common: build missing packages for camera

--- kona.mk
+++ kona.mk
@@ -205,7 +205,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera2ndk_vendor \
+    libdng_sdk.vendor \
     libgui_vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 PRODUCT_PACKAGES += \
