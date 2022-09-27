b9e2d6bab8a1148bab47eb4df7fff387201d75e5
spkal01 <kalligeross@gmail.com>
2022-09-27 21:15:37 +0300

    sm8250-common: libinit: Add support of .mod_device hides
    * Required hide for Miui Apps
    * This is second part of VoidUI-Devices/device_xiaomi_alioth@e537e1c2c23da827fd73d2d205f90673eb7db519

--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -32,6 +32,7 @@ void set_variant_props(const variant_info_t variant) {
     set_ro_build_prop("brand", variant.brand, true);
     set_ro_build_prop("device", variant.device, true);
     set_ro_build_prop("marketname", variant.marketname, true);
+    set_ro_build_prop("mod_device", variant.mod_device, true);
     set_ro_build_prop("model", variant.model, true);
     property_override("vendor.usb.product_string", variant.marketname, true);
 
