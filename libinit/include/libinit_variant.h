bb588e3fac1aa08f3353ae9304ff3d7c4d9889f8
spkal01 <kalligeross@gmail.com>
2022-09-27 21:15:37 +0300

    sm8250-common: libinit: Add support of .mod_device hides
    * Required hide for Miui Apps
    * This is second part of VoidUI-Devices/device_xiaomi_alioth@e537e1c2c23da827fd73d2d205f90673eb7db519

--- libinit/include/libinit_variant.h
+++ libinit/include/libinit_variant.h
@@ -17,6 +17,7 @@ typedef struct variant_info {
     std::string brand;
     std::string device;
     std::string marketname;
+    std::string mod_device;
     std::string model;
     std::string build_fingerprint;
 
