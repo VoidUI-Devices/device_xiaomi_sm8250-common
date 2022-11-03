0799ae93ec3c49a44bb0f433c1be3b4574d8f86f
johnmart19 <johnivan19999@gmail.com>
2022-11-03 21:35:01 +0200

    sm8250-common: Nuke OTA Preopt Script
    - OTA Dexopt optimizations taking a lot of time, let's skip it on installation moment

--- kona.mk
+++ kona.mk
@@ -74,12 +74,6 @@ PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
 AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_vendor=true \
     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
     FILESYSTEM_TYPE_vendor=ext4 \
@@ -91,8 +85,7 @@ PRODUCT_PACKAGES += \
     update_verifier
 
 PRODUCT_PACKAGES += \
-    checkpoint_gc \
-    otapreopt_script
+    checkpoint_gc
 endif
 
 # ART Debugging (Disable)
