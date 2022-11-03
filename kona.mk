8ff6aad5dbccb8e4189e892ea2d573125575e982
johnmart19 <johnivan19999@gmail.com>
2022-11-03 21:40:13 +0200

    sm8250-common: Enable ART Flags for A/B as per Google Recommendations

--- kona.mk
+++ kona.mk
@@ -86,6 +86,16 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     checkpoint_gc
+
+# Background dexopt OTA
+PRODUCT_VENDOR_PROPERTIES += \
+    pm.dexopt.ab-ota=speed-profile
+
+# Preopted ODEX files (system_other)
+PRODUCT_PACKAGES += \
+    cppreopts.sh
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.cp_system_other_odex=1
 endif
 
 # ART Debugging (Disable)
