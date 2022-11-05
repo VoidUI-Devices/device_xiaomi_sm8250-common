5f4d54f8b32d42904a3106c61144ec9423750688
johnmart19 <johnivan19999@gmail.com>
2022-11-05 07:49:10 +0200

    sm8250-common: Add support of Miui Dolby and MiSound
    Usage:
    
    If wanted to use with Dolby:
    Add TARGET_USES_MIUI_DOLBY := true to device tree, Import Dolby engine and Dolby codecs
    
    Otherwise:
    Will be available MiSound (Dirac) as default audio engine
    
    - Dolby Config Decommonized to device specific DT
    - Sepolicy is reworked and moved to sepolicy/dolby with enabling to build only when TARGET_USES_MIUI_DOLBY is true
    - Added creation of data/vendor/dolby and multimedia folders on early boot
    - Imported missing soundfx modules and import AudioSphere deps
    - Amount of props is exactly minimal for proper work of requested stuffs

--- kona.mk
+++ kona.mk
@@ -197,6 +197,39 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+ifeq ($(TARGET_USES_MIUI_DOLBY),true)
+# Miui Dolby Engine Topic
+# Dolby Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/dolby
+
+# Dolby Props
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.dolby.dax.version=DAX3_3.6.1.6_r1 \
+    ro.vendor.audio.dolby.dax.support=true \
+    ro.vendor.audio.dolby.surround.enable=true
+
+# Dolby Permissions
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/dolby/permissions,$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions)
+
+# MiSound with Dolby Environment (By Default - Disabled)
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.audio.misound.disable=true \
+    ro.vendor.audio.misound.bluetooth.enable=true
+
+# Dolby MediaCodecs Loading Support (Overwrites Vendor files)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/dolby/media/media_codecs_kona_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_kona_vendor.xml \
+    $(LOCAL_PATH)/dolby/media/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
+else
+# MiSound (Dirac Only)
+# MiSound without Dolby (By Default - Enabled)
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.audio.misound.disable=false \
+    ro.vendor.audio.misound.bluetooth.enable=true
+endif
+
+
 # Display
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer-service
