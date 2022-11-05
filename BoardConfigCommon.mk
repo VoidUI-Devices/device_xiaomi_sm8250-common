0a6ddca0caf9e5dd64ebdf2eccb12abe36453218
johnmart19 <johnivan19999@gmail.com>
2022-11-05 04:27:18 +0200

    sm8250-common: Adapt for VoidUI Project

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -225,7 +225,7 @@ endif
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/aosp/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += nfc
