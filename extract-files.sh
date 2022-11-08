69bdef6a6fc5ec53f9f60c7e5f905b370e568465
johnmart19 <johnivan19999@gmail.com>
2022-11-08 03:55:31 +0200

    sm8250-common: Import and adapt Xiaomi VibratorFeature
    This change includes:
    - Sepolicy ports from Miui for Xiaomi Vibrator and VibratorFeature hals
    - Import needed Prebuilts of AIDL service and required deps

--- extract-files.sh
+++ extract-files.sh
@@ -67,6 +67,10 @@ function blob_fixup() {
         vendor/etc/media_codecs_kona.xml)
             sed -i "/media_codecs_dolby_audio.xml/d" "${2}"
             ;;
+        vendor/bin/hw/vendor.xiaomi.hardware.vibratorfeature.service)
+            "${PATCHELF}" --replace-needed "android.hardware.vibrator-V1-ndk_platform.so" "android.hardware.vibrator-V1-ndk.so" "${2}"
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+            ;;
     esac
 }
 
--- kona.mk
+++ kona.mk
@@ -518,6 +518,19 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndservicemanager
 
+# Vibrator Configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
+# Vibrator Xiaomi
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator-V1-ndk.vendor
+
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libutils-v32.so \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v32.so
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
