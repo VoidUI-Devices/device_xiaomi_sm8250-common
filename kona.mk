d2a9ae3180475fc90fab734d4f5da8000b32340b
johnmart19 <johnivan19999@gmail.com>
2022-11-10 13:59:49 +0200

    sm8250-common: Drop QTI Vibrator compilation
    - This commit reverts branch of commits according to Vibrator changes
    0779e4cf10e887abb2ffbf5b1ac777362937ca7a
    6d73cb4b0073053cc06bf7383bf9dbf7d35d083d
    62323bdf4bdb8c1e49efc9b282f8961a2a24ebb5
    050534b56c1ea4924960c760ce92d8d22bb45beb
    ee3f90272379226ad5513f62c0d7c5f1d5fc936b
    811d062376604f5b5ff5fc28a3e9f5b1d0fda5fd
    1e89b0a3ef2cb83ca4cc4dc394f7c4ae92b65ba9
    10bd45818942ed1510ca50883652bfe8ca3b1d87
    3921eff0513b2c5eb6cd6998cc390b17658e1e42
    e5f7c5e73c023c7aeddb9885474a084395dec0fb
    20967b264daadbf99c516cf1d37cc8447f8aa7a9

--- kona.mk
+++ kona.mk
@@ -482,10 +482,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndservicemanager
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.xiaomi_kona
-
+# Vibrator Configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
