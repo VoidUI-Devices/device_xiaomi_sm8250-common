f271c16652717ec3d1956c9939d73abea3ed40bc
johnmart19 <johnivan19999@gmail.com>
2022-12-11 10:14:18 +0200

    sm8250-common: Switch to Audio Hal v7.0-7.1
    - Reference https://github.com/VoidUI-Tiramisu/frameworks_av/blob/1d15f539471e89bcaf3582c4be46a0a3e3069136/media/libaudiohal/impl/Android.bp#L184
    
    - Also declare compile out libaudiohal@7.0 as per VoidUI-Tiramisu/frameworks_av@288a3436441bccc6de61750afba3160e074166f1
    - Also keep 6.0 fallback for hardcoded libs
    
    - Test:
    Selected targets compiled out to system & vendor & vendor/lib(64)/hw partitions and paths
    Device booted, sound works, effects works fine (Dolby, MiSound and so on)

--- kona.mk
+++ kona.mk
@@ -107,6 +107,21 @@ PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.effect@6.0-util \
+    libaudiohal@7.0 \
+    libaudiohal@7.1 \
+    android.hardware.audio.common@7.0 \
+    android.hardware.audio.common@7.0-util \
+    android.hardware.audio.common@7.1-enums \
+    android.hardware.audio.common@7.1-util \
+    android.hardware.audio.effect@7.0 \
+    android.hardware.audio.effect@7.0-util \
+    android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio@7.0 \
+    android.hardware.audio@7.1 \
+    android.hardware.audio@7.1-util \
+    android.hardware.audio@7.1-impl \
+    libaudiohal.effect@7.0 \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.3-impl
@@ -161,7 +176,7 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
