a9163a87761f27a679a452c0b785aa2878680734
johnmart19 <johnivan19999@gmail.com>
2022-12-26 15:10:39 +0200

    sm8250-common: Dolby: Use correct prop for android 13
    - Updated prop is ro.vendor.audio.dolby.dax.version
    - This is matching EffectDapController to preload properties correctly

--- kona.mk
+++ kona.mk
@@ -204,7 +204,7 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/dolby
 
 # Dolby Props
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.dolby.dax.version=DAX3_3.6.1.6_r1 \
+    ro.vendor.audio.dolby.dax.version=DAX3_3.6.1.6_r1 \
     ro.vendor.audio.dolby.dax.support=true \
     ro.vendor.audio.dolby.surround.enable=true
 
