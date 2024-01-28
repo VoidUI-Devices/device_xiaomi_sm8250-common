b86b65da83bab55c07f20f63e953534fa020f1e0
johnmart19 <johnivan19999@gmail.com>
2024-01-28 18:18:41 +0200

    sm8250-common: Sepolicy: Import minimal Mi_thermald changes from AOSPA Trees
    Based on changes:
    UtsavBalar1231/platform_device_xiaomi_alioth@26b8ad1
    UtsavBalar1231/platform_device_xiaomi_alioth@051ae1d
    
    - Contains only minimal changes with cleanup
    - Not allowed to use general sysfs as it is not needed
    Reviewed via Ghidra:
     Requested access to:
       /sys/class/thermal
       /sys/class/power_supply
       /sys/class/backlight
       /sys/class/leds
    
    Co-authored-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kona.mk
+++ kona.mk
@@ -503,6 +503,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+# Thermal Mi_Thermald
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.sys.thermal.data.path=/data/vendor/thermal/
+
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.xiaomi
